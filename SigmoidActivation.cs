33b96a58e20a2352fb131e7027ffaf2ca242ca45
kasla <kasla@DESKTOP-SR937O1>
2024-01-21 04:05:42 -0800

    Uses IActivation to calculate the output

--- SigmoidActivation.cs
+++ SigmoidActivation.cs
@@ -1,6 +1,16 @@
 ﻿namespace NNdotnet
 {
-    public class SigmoidActivation
+    public class SigmoidActivation : IActivation
     {
+        private double _coeficient;
+
+        public SigmoidActivation(double coeficient)
+        {
+            _coeficient = coeficient;
+        }
+        public double CalculateOutput(double weightedInput)
+        {
+            return (1 / (1 + Math.Exp(-weightedInput * _coeficient)));
+        }
     }
 }
