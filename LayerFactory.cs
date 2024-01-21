c5d733fe4b6eafa94e2776f08e3c18e08ecddfdb
kasla <kasla@DESKTOP-SR937O1>
2024-01-21 04:03:53 -0800

    LayerFactory of Layer classes

---
+++ LayerFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace NNdotnet
+{ 
+    public class LayerFactory
+    {
+        public Layer CreateNeuralLayer(int numberOfNeurons, IActivation activationFunction, IInput inputFunction)
+        {
+            var layer = new Layer();
+            for (int i = 0; i < numberOfNeurons; i++)
+            {
+                layer.Neurons.Add(new Neuron(activationFunction, inputFunction));
+            }
+            return layer;
+        }
+    }
+}
