7e553453092e1f3c9abc81cef494ad218872fcac
kasla <kasla@DESKTOP-SR937O1>
2024-01-22 02:34:20 -0800

    LayerFactory

---
+++ src/Utilities/LayerFactory.cs
@@ -0,0 +1,18 @@
+﻿using NNdotnet.src.Interfaces;
+using NNdotnet.src.Models;
+
+namespace NNdotnet.src.Utilities
+{
+    public class LayerFactory
+    {
+        public static Layer Create(int numberOfNeurons, IActivation activationFunction, IInput inputFunction)
+        {
+            var layer = new Layer();
+            for (int i = 0; i < numberOfNeurons; i++)
+            {
+                layer.Neurons.Add(new Neuron(activationFunction, inputFunction));
+            }
+            return layer;
+        }
+    }
+}
