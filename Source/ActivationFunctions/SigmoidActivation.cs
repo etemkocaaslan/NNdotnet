f10f54715b6f7da3c54fffda761dc101ab117df0
kasla <kasla@DESKTOP-SR937O1>
2024-02-06 08:32:19 -0800

    primary constructor

--- Source/ActivationFunctions/SigmoidActivation.cs
+++ Source/ActivationFunctions/SigmoidActivation.cs
@@ -2,17 +2,14 @@
 
 namespace NNdotnet.src.ActivationFunctions
 {
-    public class SigmoidActivation : IActivation
+    public class SigmoidActivation(double coeficient) : IActivation
     {
-        private double _coeficient;
-
-        public SigmoidActivation(double coeficient)
-        {
-            _coeficient = coeficient;
-        }
+        private double _coeficient = coeficient;
         public double CalculateOutput(double weightedInput)
         {
-            return 1 / (1 + Math.Exp(-weightedInput * _coeficient));
+            return 1 / (1 + Math.Exp(-weightedInput * Coeficient));
         }
+
+        public double Coeficient { get => _coeficient; set => _coeficient = value; }
     }
 }
